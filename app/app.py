a0f2ff09e9aeed487d7bd48178e66a7d110758f9
Sumit Kumar <timus@gatech.edu>
2018-11-08 15:45:10 -0500

    Add login and templates for home and map_table page

--- app/app.py
+++ app/app.py
@@ -1,16 +1,34 @@
-from flask import Flask, render_template, g, request, redirect, url_for, flash
+from flask import Flask, render_template, g, request, redirect, url_for, flash, session
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 import psycopg2
 import difflib
 import itertools
 from collections import defaultdict
+from functools import wraps
 
 # Import dictionaries: Patient, HumanName, ContactPoint, Address, Period, etc.
 from constants import *
 
+# Used to track changes between builds
+version = 0.001
+
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d444427567d441f2b6176a'
+
+
+""" Section: Helper functions """
+
+
+def login_required(f):
+    # Redirect to the login page, if not logged-in.
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect(url_for('login'))
+    return wrap
 
 
 def test_get_db():
@@ -26,6 +44,29 @@ def test_get_db():
     return g.db
 
 
+""" Section: Routes """
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == "demo" and password == "password!23":
+            print "DEBUG: User logged in"
+            session['logged_in'] = True
+            return redirect(url_for('home'))
+        else:
+            return render_template("login.html", error="Incorrect Credentials")
+    return render_template("login.html", error="")
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('root'))
+
+
 @app.teardown_appcontext
 def close_db(error):
     """
@@ -36,18 +77,64 @@ def close_db(error):
 
 
 @app.route('/')
-def show_info():
-    cur = test_get_db().cursor()
-    print "Debug: Database connection established"
-    cur.close()
-    print "Debug: Database connection closed"
-    return "Successfully established and closed DB connection! Hello world!"
+def root():
+    return redirect(url_for('home'))
 
 
 class ReusableForm(Form):
     url = TextField('Database Endpoint:', validators=[validators.required()])
     username = TextField('Username:', validators=[validators.required()])
     password = TextField('Password:', validators=[validators.required()])
+@app.route('/home', methods=['GET', 'POST'])
+@login_required
+def home():
+    return render_template("home.html", version=version)
+
+
+@app.route('/map_table', methods=['GET', 'POST'])
+@login_required
+def map_table():
+    if request.method == 'POST':
+
+        # If user is trying to get mapping
+        if 'database_endpoint' in request.form:
+
+            url = request.form['database_endpoint']
+            username = request.form['database_username']
+            password = request.form['database_password']
+            print url
+            print username
+            print password
+
+            flash('Pulling database tables...', 'info')
+
+            # Pull database column names for table
+            # conn = psycopg2.connect('host=data.hdap.gatech.edu port=5433 dbname=mimic_v5 user=team0 password=hdapM1m1c4Students!')
+            database_string = 'host=' + url + ' port=5433' + ' dbname=mimic_v5 user=' + username + ' password=' + password
+            conn = psycopg2.connect(database_string)
+            cur = conn.cursor()
+            cur.execute("SELECT * FROM mimic_v5.f_person LIMIT 0")
+            cur.close()
+            colnames = [name[0] for name in cur.description]
+
+            best_mappings = {}
+            allFhirFields = list(
+                itertools.chain(Patient.keys(), HumanName.keys(), ContactPoint.keys(), Address.keys(), Period.keys()))
+
+            # Iterate through all database column names and find best match to FHIR data type
+            for name in colnames:
+                if len(difflib.get_close_matches(name, allFhirFields)) == 0:
+                    best_mappings[name] = 'UNKNOWN'
+                else:
+                    best_mappings[name] = difflib.get_close_matches(name, allFhirFields, 1)[0]
+
+            flash(best_mappings, 'mappings')
+            flash(allFhirFields, 'fhirfields')
+        else:
+            # Re-direct to home if the form was not sent properly
+            return redirect(url_for('home'))
+
+    return render_template("table.html")
 
 
 @app.route('/map', methods=['GET', 'POST'])
